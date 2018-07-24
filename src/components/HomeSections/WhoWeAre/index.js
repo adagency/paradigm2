b5371bb707282c719c0bb3d832767e13c3ea2d0c
juanortiz10 <juanjo.jjo94@gmail.com>
2018-07-24 09:42:57 -0500

    Window bug fixed

--- src/components/HomeSections/WhoWeAre/index.js
+++ src/components/HomeSections/WhoWeAre/index.js
@@ -23,8 +23,14 @@ export default class WhoWeAreSlide extends Component {
 			sectionPosition: 0,
 			timesAnimated: 0,
 			onEntering: false,
-			width: window.innerWidth
+			width: 0
 		};
+	}
+
+	componentDidMount() {
+		this.setState({
+			width: window.innerWidth
+		});
 	}
 
 	componentDidUpdate() {
