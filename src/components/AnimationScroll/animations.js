575b515153375f027cf0f669bb6be1fda36c5992
miltondaniel29 <miltondaniel29.mdf@gmail.com>
2018-07-25 10:35:19 -0500

    chore: add animation to the titles of services

--- src/components/AnimationScroll/animations.js
+++ src/components/AnimationScroll/animations.js
@@ -33,6 +33,28 @@ export const imageRight = {
   to: { transformPerspective: 1000, rotationY: 0, x: '0%', z: 0, transformOrigin: 'left', opacity: 1 },
 }
 
+export const slidein = {
+  from: {
+    marginLeft: '40%',
+    width: '100%',
+    color:'#203040'
+  },
+  to: {
+      marginLeft: '0%',
+      width: '100%',
+      color:'#56a6c9'
+  }
+}
+
+export const rotation = {
+  from: {
+    rotation:0
+  },
+  to: {
+    rotation:360
+  }
+}
+
 export const gallery = {
   from: { opacity: 0 },
   to: { opacity: 1 }
--- src/pages/services/index.js
+++ src/pages/services/index.js
@@ -35,14 +35,19 @@ export default function ServicesPage({ data }) {
         <Content padding='medium'>
           <AnimScroll types='fadeIn'>
           <Col size='two-third'>
-            <AnimScroll types='imageRight' duration={3}>
+
+
+            <AnimScroll types='slidein' duration={1}>
               <h3 className={styles.title}>
-                  <span className={styles.imgContainer}>
+                <span className={styles.imgContainer}>
+                  <AnimScroll types='rotation' duration={1}>
                     <img src={preConstruction} className={styles.img} alt='Pre-construction' />
-                  </span>
-                Pre-construction
+                  </AnimScroll>
+                </span>
+                  Pre-construction
               </h3>
             </AnimScroll>
+            <AnimScroll types='fadeIn'>
             <p>
               We are not just contractors, we are your collaborative partners. Our
               pre-construction phase involves design with our architects, and
@@ -50,100 +55,129 @@ export default function ServicesPage({ data }) {
               logistics prior to start up, ensuring that everything moves as smoothly
               as possible.
             </p>
-            <AnimScroll types='imageRight' duration={3}>
+            </AnimScroll>
+
+
+            <AnimScroll types='slidein' duration={1}>
               <h3 className={styles.title}>
                 <span className={styles.imgContainer}>
-                  <img src={scheduling} className={styles.img} alt='Scheduling' />
+                  <AnimScroll types='rotation' duration={1}>
+                    <img src={scheduling} className={styles.img} alt='Scheduling' />
+                  </AnimScroll>
                 </span>
                 Scheduling
               </h3>
             </AnimScroll>
-            <p>
-              We employ strict scheduling - from site analysis to product selections
-              and purchasing, and design efficiencies, we’ll take care of every
-              detail and are with you at every step. When the project is clearly
-              defined through pre construction, reliable project schedules are met
-              at every level. Unquestionably everything matters.
-            </p>
-            <AnimScroll types='imageRight' duration={3}>
+            <AnimScroll types='fadeIn'>
+              <p>
+                We employ strict scheduling - from site analysis to product selections
+                and purchasing, and design efficiencies, we’ll take care of every
+                detail and are with you at every step. When the project is clearly
+                defined through pre construction, reliable project schedules are met
+                at every level. Unquestionably everything matters.
+              </p>
+            </AnimScroll>
+
+            <AnimScroll types='slidein' duration={1}>
               <h3 className={styles.title}>
                 <span className={styles.imgContainer}>
-                  <img src={safety} className={styles.img} alt='Safety' />
+                  <AnimScroll types='rotation' duration={1}>
+                    <img src={safety} className={styles.img} alt='Safety' />
+                  </AnimScroll>
                 </span>
                 Safety
               </h3>
             </AnimScroll>
-            <p>
-              Paradigms commitment to safety is unparalleled. We develop a
-              comprehensive safety plan for the entire project, with the client,
-              before we even break ground. This plan includes thorough inspection
-              processes, daily evaluations and ongoing training for our
-              superintendents to ensure that our employees, subcontractors and
-              our clients go home safely every day.
-            </p>
-            <AnimScroll types='imageRight' duration={3}>
+            <AnimScroll types='fadeIn'>
+              <p>
+                Paradigms commitment to safety is unparalleled. We develop a
+                comprehensive safety plan for the entire project, with the client,
+                before we even break ground. This plan includes thorough inspection
+                processes, daily evaluations and ongoing training for our
+                superintendents to ensure that our employees, subcontractors and
+                our clients go home safely every day.
+              </p>
+            </AnimScroll>
+
+            <AnimScroll types='slidein' duration={1}>
               <h3 className={styles.title}>
                 <span className={styles.imgContainer}>
-                  <img src={postConstruction} className={styles.img} alt='Post-scheduling' />
+                  <AnimScroll types='rotation' duration={1}>
+                    <img src={postConstruction} className={styles.img} alt='Post-scheduling' />
+                  </AnimScroll>
                 </span>
                 Post-Construction
               </h3>
             </AnimScroll>
-            <p>
-              Your goals are our goals. It takes supreme knowledge of complex
-              construction methods and a methodical approach to hold each team
-              member accountable throughout the construction process. Your
-              project isn't complete until every last detail is achieved.
-            </p>
-            <h4>Our post-construction services include: </h4>
-            <ul>
-              <li>Operation and Maintenance manual development</li>
-              <li>As-Built Drawing documentation</li>
-              <li>Lien Waivers/Releases settlement</li>
-              <li>Coordination of FF&E Delivery and Installation</li>
-              <li>Coordination of Move-In</li>
-              <li>Warranties</li>
-              <li>Assemble “As Builts” and Owner’s Manual</li>
-              <li>Project Close- Out</li>
-            </ul>
-            <AnimScroll types='imageRight' duration={3}>
+            <AnimScroll types='fadeIn'>
+              <p>
+                Your goals are our goals. It takes supreme knowledge of complex
+                construction methods and a methodical approach to hold each team
+                member accountable throughout the construction process. Your
+                project isn't complete until every last detail is achieved.
+              </p>
+              <h4>Our post-construction services include: </h4>
+              <ul>
+                <li>Operation and Maintenance manual development</li>
+                <li>As-Built Drawing documentation</li>
+                <li>Lien Waivers/Releases settlement</li>
+                <li>Coordination of FF&E Delivery and Installation</li>
+                <li>Coordination of Move-In</li>
+                <li>Warranties</li>
+                <li>Assemble “As Builts” and Owner’s Manual</li>
+                <li>Project Close- Out</li>
+              </ul>
+              <br/>
+            </AnimScroll>
+
+            <AnimScroll types='slidein' duration={1}>
               <h3 className={styles.title}>
                 <span className={styles.imgContainer}>
-                  <img src={bimModeling} className={styles.img} alt='Post-scheduling' />
+                  <AnimScroll types='rotation' duration={1}>
+                    <img src={bimModeling} className={styles.img} alt='Post-scheduling' />
+                  </AnimScroll>
                 </span>
                 BIM Modeling
               </h3>
             </AnimScroll>
-            <p>
-              BIM otherwise known as Building information modeling involves more
-              than just 3D design, it is the process of creating and managing all
-              project information from before, during, and after construction.
-              Paradigm utilizes BIM modeling to resolve any unforeseen conflicts
-              before they arise, and to monitor productivity of the construction
-              process.
-            </p>
-            <AnimScroll types='imageRight' duration={3}>
+            <AnimScroll types='fadeIn'>
+              <p>
+                BIM otherwise known as Building information modeling involves more
+                than just 3D design, it is the process of creating and managing all
+                project information from before, during, and after construction.
+                Paradigm utilizes BIM modeling to resolve any unforeseen conflicts
+                before they arise, and to monitor productivity of the construction
+                process.
+              </p>
+            </AnimScroll>
+
+            <AnimScroll types='slidein' duration={1}>
               <h3 className={styles.title}>
                 <span className={styles.imgContainer}>
-                  <img src={deliveryMethods} className={styles.img} alt='Post-scheduling' />
+                  <AnimScroll types='rotation' duration={1}>
+                    <img src={deliveryMethods} className={styles.img} alt='Post-scheduling' />
+                  </AnimScroll>
                 </span>
                 Delivery Methods
               </h3>
             </AnimScroll>
-            <p>
-              Our delivery methods help to reduce risk as well as keep the project
-              on time and budget. Discussing the projects needs and specific
-              situations with our clients before startup ensures that we have all
-              details aligned, goals are met, and quality remains superior.
-            </p>
-            <h5>Our Delivery Methods include:</h5>
-            <ul>
-              <li>General Contracting</li>
-              <li>Competitive Sealed Proposal (CSP)</li>
-              <li>Construction Manager at Risk (CMAR)</li>
-              <li>Design Build</li>
-              <li>Fee with Negotiated GMP</li>
-            </ul>
+            <AnimScroll types='fadeIn'>
+              <p>
+                Our delivery methods help to reduce risk as well as keep the project
+                on time and budget. Discussing the projects needs and specific
+                situations with our clients before startup ensures that we have all
+                details aligned, goals are met, and quality remains superior.
+              </p>
+              <h5>Our Delivery Methods include:</h5>
+              <ul>
+                <li>General Contracting</li>
+                <li>Competitive Sealed Proposal (CSP)</li>
+                <li>Construction Manager at Risk (CMAR)</li>
+                <li>Design Build</li>
+                <li>Fee with Negotiated GMP</li>
+              </ul>
+            </AnimScroll>
+
           </Col>
           </AnimScroll>
         </Content>
