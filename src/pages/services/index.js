36796ab25be628a178d6f37aadbc566921f54533
miltondaniel29 <miltondaniel29.mdf@gmail.com>
2018-07-24 21:27:10 -0500

    chore: change services icons

--- src/pages/services/index.js
+++ src/pages/services/index.js
@@ -9,10 +9,12 @@ import styles from './styles.module.scss'
 
 import Blueprint from 'components/BlueprintSVGs'
 
-import postConstruction from './post-construction.png'
-import preConstruction from './pre-construction.png'
-import safety from './safety.png'
-import scheduling from './scheduling.png'
+import postConstruction from './postconstruction-icon.png'
+import preConstruction from './preconstruction-icon.png'
+import safety from './safety-icon.png'
+import scheduling from './scheduling-icon.png'
+import bimModeling from './bim-icon.png'
+import deliveryMethods from './delivery-icons.png'
 
 export default function ServicesPage({ data }) {
   return (
@@ -31,97 +33,119 @@ export default function ServicesPage({ data }) {
           <Blueprint svg='Services' />
         </div>
         <Content padding='medium'>
+          <AnimScroll types='fadeIn'>
           <Col size='two-third'>
-            <AnimScroll>
+            <AnimScroll types='imageRight' duration={3}>
               <h3 className={styles.title}>
                   <span className={styles.imgContainer}>
                     <img src={preConstruction} className={styles.img} alt='Pre-construction' />
                   </span>
                 Pre-construction
               </h3>
-              <p>
-                We are not just contractors, we are your collaborative partners. Our
-                pre-construction phase involves design with our architects, and
-                evaluations of the project so that we can coordinate all timelines and
-                logistics prior to start up, ensuring that everything moves as smoothly
-                as possible.
-              </p>
+            </AnimScroll>
+            <p>
+              We are not just contractors, we are your collaborative partners. Our
+              pre-construction phase involves design with our architects, and
+              evaluations of the project so that we can coordinate all timelines and
+              logistics prior to start up, ensuring that everything moves as smoothly
+              as possible.
+            </p>
+            <AnimScroll types='imageRight' duration={3}>
               <h3 className={styles.title}>
                 <span className={styles.imgContainer}>
                   <img src={scheduling} className={styles.img} alt='Scheduling' />
                 </span>
                 Scheduling
               </h3>
-              <p>
-                We employ strict scheduling - from site analysis to product selections
-                and purchasing, and design efficiencies, we’ll take care of every
-                detail and are with you at every step. When the project is clearly
-                defined through pre construction, reliable project schedules are met
-                at every level. Unquestionably everything matters.
-              </p>
+            </AnimScroll>
+            <p>
+              We employ strict scheduling - from site analysis to product selections
+              and purchasing, and design efficiencies, we’ll take care of every
+              detail and are with you at every step. When the project is clearly
+              defined through pre construction, reliable project schedules are met
+              at every level. Unquestionably everything matters.
+            </p>
+            <AnimScroll types='imageRight' duration={3}>
               <h3 className={styles.title}>
                 <span className={styles.imgContainer}>
                   <img src={safety} className={styles.img} alt='Safety' />
                 </span>
                 Safety
               </h3>
-              <p>
-                Paradigms commitment to safety is unparalleled. We develop a
-                comprehensive safety plan for the entire project, with the client,
-                before we even break ground. This plan includes thorough inspection
-                processes, daily evaluations and ongoing training for our
-                superintendents to ensure that our employees, subcontractors and
-                our clients go home safely every day.
-              </p>
+            </AnimScroll>
+            <p>
+              Paradigms commitment to safety is unparalleled. We develop a
+              comprehensive safety plan for the entire project, with the client,
+              before we even break ground. This plan includes thorough inspection
+              processes, daily evaluations and ongoing training for our
+              superintendents to ensure that our employees, subcontractors and
+              our clients go home safely every day.
+            </p>
+            <AnimScroll types='imageRight' duration={3}>
               <h3 className={styles.title}>
                 <span className={styles.imgContainer}>
                   <img src={postConstruction} className={styles.img} alt='Post-scheduling' />
                 </span>
                 Post-Construction
               </h3>
-              <p>
-                Your goals are our goals. It takes supreme knowledge of complex
-                construction methods and a methodical approach to hold each team
-                member accountable throughout the construction process. Your
-                project isn't complete until every last detail is achieved.
-              </p>
-              <h4>Our post-construction services include: </h4>
-              <ul>
-                <li>Operation and Maintenance manual development</li>
-                <li>As-Built Drawing documentation</li>
-                <li>Lien Waivers/Releases settlement</li>
-                <li>Coordination of FF&E Delivery and Installation</li>
-                <li>Coordination of Move-In</li>
-                <li>Warranties</li>
-                <li>Assemble “As Builts” and Owner’s Manual</li>
-                <li>Project Close- Out</li>
-              </ul>
-              <h4>BIM Modeling</h4>
-              <p>
-                BIM otherwise known as Building information modeling involves more
-                than just 3D design, it is the process of creating and managing all
-                project information from before, during, and after construction.
-                Paradigm utilizes BIM modeling to resolve any unforeseen conflicts
-                before they arise, and to monitor productivity of the construction
-                process.
-              </p>
-              <h4>Delivery Methods</h4>
-              <p>
-                Our delivery methods help to reduce risk as well as keep the project
-                on time and budget. Discussing the projects needs and specific
-                situations with our clients before startup ensures that we have all
-                details aligned, goals are met, and quality remains superior.
-              </p>
-              <h5>Our Delivery Methods include:</h5>
-              <ul>
-                <li>General Contracting</li>
-                <li>Competitive Sealed Proposal (CSP)</li>
-                <li>Construction Manager at Risk (CMAR)</li>
-                <li>Design Build</li>
-                <li>Fee with Negotiated GMP</li>
-              </ul>
             </AnimScroll>
+            <p>
+              Your goals are our goals. It takes supreme knowledge of complex
+              construction methods and a methodical approach to hold each team
+              member accountable throughout the construction process. Your
+              project isn't complete until every last detail is achieved.
+            </p>
+            <h4>Our post-construction services include: </h4>
+            <ul>
+              <li>Operation and Maintenance manual development</li>
+              <li>As-Built Drawing documentation</li>
+              <li>Lien Waivers/Releases settlement</li>
+              <li>Coordination of FF&E Delivery and Installation</li>
+              <li>Coordination of Move-In</li>
+              <li>Warranties</li>
+              <li>Assemble “As Builts” and Owner’s Manual</li>
+              <li>Project Close- Out</li>
+            </ul>
+            <AnimScroll types='imageRight' duration={3}>
+              <h3 className={styles.title}>
+                <span className={styles.imgContainer}>
+                  <img src={bimModeling} className={styles.img} alt='Post-scheduling' />
+                </span>
+                BIM Modeling
+              </h3>
+            </AnimScroll>
+            <p>
+              BIM otherwise known as Building information modeling involves more
+              than just 3D design, it is the process of creating and managing all
+              project information from before, during, and after construction.
+              Paradigm utilizes BIM modeling to resolve any unforeseen conflicts
+              before they arise, and to monitor productivity of the construction
+              process.
+            </p>
+            <AnimScroll types='imageRight' duration={3}>
+              <h3 className={styles.title}>
+                <span className={styles.imgContainer}>
+                  <img src={deliveryMethods} className={styles.img} alt='Post-scheduling' />
+                </span>
+                Delivery Methods
+              </h3>
+            </AnimScroll>
+            <p>
+              Our delivery methods help to reduce risk as well as keep the project
+              on time and budget. Discussing the projects needs and specific
+              situations with our clients before startup ensures that we have all
+              details aligned, goals are met, and quality remains superior.
+            </p>
+            <h5>Our Delivery Methods include:</h5>
+            <ul>
+              <li>General Contracting</li>
+              <li>Competitive Sealed Proposal (CSP)</li>
+              <li>Construction Manager at Risk (CMAR)</li>
+              <li>Design Build</li>
+              <li>Fee with Negotiated GMP</li>
+            </ul>
           </Col>
+          </AnimScroll>
         </Content>
       </div>
       <Industries />
